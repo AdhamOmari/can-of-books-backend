c642fc835c6c242182b867434d81e2a6df474d8d
qusaiqeisi <qusaiqeisi6@gmail.com>
2021-07-08 18:21:29 +0300

    class 13 yet

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -15,4 +15,48 @@ const booksShow=(req,res)=>{
     })
 
 }
-module.exports=booksShow
+
+
+// add new book 
+
+const addBook=(req,res)=>{
+    const {email,name,description,status}= req.body;
+    
+    userModel.findOne({email:userEmail}, (error, user)=>{
+        if(error){
+            res.send('error');
+        }else{
+            user.arryBooks.push({
+                name:name,
+                description:description,
+                status:status
+            })
+            user.save();
+            res.send(user.arryBooks);
+        }
+    })
+        
+}
+
+
+// delete book
+const deleteBook=(req,res)=>{
+    const email=req.query.email;
+    const book_id=Number(req.params.index);
+    console.log(book_id);
+    userModels.findOne({email:email}, (error, user)=>{
+        let newBookArra=[];
+        user.arryBooks.forEach((el,idx)=>{
+            if(idx!==book_id){
+                newBookArra.push(el);
+            }
+        })
+        user.arryBooks=newBookArra;
+        user.save();
+        res.send( user.arryBooks)
+    });
+
+}
+
+
+module.exports={booksShow,addBook,deleteBook};
